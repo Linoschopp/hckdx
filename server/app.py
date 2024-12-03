92b14c091e3b0650eae9c43350c3be855c7fc93c
Lino Schopp <lino.elias.schopp@icloud.com>
2024-12-03 15:28:38 +0100

    Add server(app.py)

--- server/app.py
+++ server/app.py
@@ -1,127 +1,72 @@
-from operator import attrgetter
+import json
+from pathlib import Path
+from flask import Flask, request
 
-from flask import Flask, make_response, request
-import management
-import jwt
-
-from management import Device
-
-SECRET = "iCH hAcKe scHuLcomPuTer, aBer das DaRf niemAnd wIsSen!1!"
+PASSWORD = "hi"
 DEVICES_STORAGE_LOCATION = "devices.txt"
-DEVICE_PREFIX = "DEV_"
 
-devices = management.DeviceStorage.load_devices(DEVICES_STORAGE_LOCATION)
+
+def load(devices_storage_location):
+    if Path(devices_storage_location).exists():
+        with open(devices_storage_location, "r") as file:
+            namelist = json.load(file)
+            return dict(zip(namelist, map(lambda x: False, namelist)))
+    else:
+        return {}
+
 
 app = Flask(__name__)
+devices = load(DEVICES_STORAGE_LOCATION)
+
+
+def save(devices):
+    with open(DEVICES_STORAGE_LOCATION, "w") as file:
+        json.dump(list(devices.keys()), file)
 
 
 @app.post("/register")
 def register():
-    hostname = request.get_data().decode()
-    token = jwt.encode({"sub": DEVICE_PREFIX + hostname}, SECRET, "HS256")
-    devices.devices.append(Device(hostname))
-    return token
+    hostname = request.headers.get("Device")
+    devices[hostname] = False
+    save(devices)
+    return "Okay"
+
+
+@app.post("/delete")
+def delete():
+    if request.headers.get("Password") == PASSWORD:
+        hostname = request.headers.get("Device")
+        del devices[hostname]
+        save(devices)
+        return "Okay"
+    return "Forbidden", 403
 
 
 @app.get("/requested")
 def requested():
     device = request.headers.get("Device")
-    return "True" if devives[device].state == "REQUESTED" else "False"
+    return str(devices.get(device))
 
 
 @app.post("/request")
-def request():
-    token = request.headers.get("Token")
-    device_hostname = request.headers.get("Device")
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-    except jwt.ExpiredSignatureError:
-        return "TOKEN EXPIRED", 401
-    except jwt.InvalidTokenError:
-        return "INVALID TOKEN", 401
-    except jwt.PyJWTError:
-        return "TOKEN-AUTH ERROR", 500
-    else:
-        if payload["sub"] != "theOnlyLino":
-            return "YOU ARE NOT LINO", 401
-        else:
-            devices[device_hostname].state = "REQUESTED"
-            return "SUCCESS"
+def set_requested():
+    if request.headers.get("Password") == PASSWORD:
+        if (device := request.headers.get("Device")) in devices:
+            devices[device] = True
+            print(devices)
+            return "Okay"
+    return "Forbidden", 403
 
 
 @app.post("/deactivate")
 def deactivate():
-    token = request.headers.get("Token")
-    device_hostname = request.headers.get("Device")
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-    except jwt.ExpiredSignatureError:
-        return "TOKEN EXPIRED", 401
-    except jwt.InvalidTokenError:
-        return "INVALID TOKEN", 401
-    except jwt.PyJWTError:
-        return "TOKEN-AUTH ERROR", 500
-    else:
-        if payload["sub"] != "theOnlyLino":
-            return "YOU ARE NOT LINO", 401
-        else:
-            devices[device_hostname].state = "INACTIVE"
-            devices[device_hostname].put(management.Command("BLANK", []))
-            return "SUCCESS"
-
-
-
-@app.post("/commands/add/")
-def add_command():
-    token = request.headers.get("Token")
-    device_hostname = request.headers.get("Device")
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-    except jwt.ExpiredSignatureError:
-        return "TOKEN EXPIRED", 401
-    except jwt.InvalidTokenError:
-        return "INVALID TOKEN", 401
-    except jwt.PyJWTError:
-        return "TOKEN-AUTH ERROR", 500
-    else:
-        if payload["sub"] != "theOnlyLino":
-            return "YOU ARE NOT LINO", 401
-        else:
-            data = request.get_data().decode()
-            cmd = management.Command.import_from_string(data)
-            print(device_hostname)
-            device = devices.get_device(device_hostname)
-            if not device:
-                print(f"Device {device_hostname} not found in devices.")
-                return "DEVICE NOT FOUND", 404
-            device.put(cmd)
-            return "SUCCESS", 200
-
-
-@app.get("/commands/get")
-def get_command():
-    token = request.headers.get("Token")
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
-    except jwt.ExpiredSignatureError:
-        return "TOKEN EXPIRED", 401
-    except jwt.InvalidTokenError:
-        return "INVALID TOKEN", 401
-    except jwt.PyJWTError:
-        return "TOKEN-AUTH ERROR", 500
-    else:
-        if not payload["sub"].startswith("DEV_"):
-            return "INVALID TOKEN", 401
-        device_hostname = payload["sub"][4:]
-        device = devices.get_device(device_hostname)
-        if not device:
-            print(f"Device {device_hostname} not found in devices.")
-            return "DEVICE NOT FOUND", 404
-        cmd = device.get()
-        return cmd.export_to_string()
+    if request.headers.get("Password") == PASSWORD:
+        if (device := request.headers.get("Device")) in devices:
+            devices[device] = False
+            return "Okay"
+    return "Forbidden", 403
 
 
 if __name__ == '__main__':
-    token = jwt.encode({"sub": "theOnlyLino"}, SECRET, algorithm="HS256")
-    print(f"Admin-Token: {token}")
+    print(f"Admin-Passwort: {PASSWORD}")
     app.run(debug=True)
